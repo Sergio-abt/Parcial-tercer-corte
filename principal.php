e04b32ac1cca18bede1fb2b459f9827aa4c29453
alison14 <47488562+alison14@users.noreply.github.com>
2021-11-24 14:45:46 -0500

    Delete principal.php

--- principal.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-
-    if(isset($_SESSION['usuario'])){
-        require 'frontend/principal-vista.php';
-    }else{
-        header ('location: login.php');
-    }
-        
-?>
